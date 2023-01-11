ed52cdd0d85aa1f63eaf478f3f145e62ccdddd00
1sreal <niibenedict123@gmail.com>
2023-01-11 01:51:30 +0000

    circle_area

--- circle_area/circleArea.py
+++ circle_area/circleArea.py
@@ -7,9 +7,11 @@ def circle_area(radius):
     area = PI*radius**2
     return area
 
-import math
+
 
 radius = int(input("Enter prefered radius: \n"))
+unit = input("What units are you working in? \n")
+unit_sqr ="{}²".format(unit)
 area = circle_area(radius)
-print(f"Area of circle with radius {radius} is {area}")
+print(f"Area of circle with radius {radius} is {area}"+ unit_sqr)
 
