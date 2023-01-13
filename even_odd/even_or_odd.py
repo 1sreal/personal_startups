20dce5958de11626f96ada2304291e5ed5204850
1sreal <niibenedict123@gmail.com>
2023-01-13 23:40:38 +0000

    even_or_odd

---
+++ even_odd/even_or_odd.py
@@ -0,0 +1,6 @@
+digit = int(input("Enter to know whether a number is odd or even: "))
+if digit % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number(" + str(digit) + ") is odd.")    
+
