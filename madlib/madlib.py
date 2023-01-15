7f42787eb113514a6cb1dfd75d2184323a80b022
1sreal <niibenedict123@gmail.com>
2023-01-15 21:38:38 +0000

    updated__madlib

--- madlib/madlib.py
+++ madlib/madlib.py
@@ -1,5 +1,17 @@
-noun1 = input("Enter a noun: ")
+"""noun1 = input("Enter a noun: ")
 verb1 = input("Enter a verb: ")
 adjective1 = input("Enter an adjective: ")
 
 print("The " + adjective1 + " " + noun1 + " " + verb1 + " through the forest.")
+"""
+noun1 = input("Enter a noun: ")
+verb1 = input("Enter a verb: ")
+adjective1 = input("Enter an adjective: ")
+noun2 = input("Enter another noun: ")
+verb2 = input("Enter another verb: ")
+adjective2 = input("Enter another adjective: ")
+
+print("The " + adjective1 + " " + noun1 + " " + verb1 + " through the " + adjective2 + " forest.")
+print("The "+ noun1 + " came across a " + noun2 + " and decided to " + verb2 + " it.")
+print("As the "+ noun1 + " " + verb2 + " the " + noun2 + " it let out a loud " + adjective2 + " scream.")
+print("But the "+ noun1 + " didn't care and continued to " + verb2 + " the " + noun2 + ".")
