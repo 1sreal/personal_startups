daeaa3ada98d1714c3a411ad4364f4231eb224af
1sreal <niibenedict123@gmail.com>
2023-01-15 21:35:07 +0000

    super_basic_madlib

---
+++ madlib/madlib.py
@@ -0,0 +1,5 @@
+noun1 = input("Enter a noun: ")
+verb1 = input("Enter a verb: ")
+adjective1 = input("Enter an adjective: ")
+
+print("The " + adjective1 + " " + noun1 + " " + verb1 + " through the forest.")
