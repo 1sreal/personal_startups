51a74d81703040abd0a745681ec6a26176c6614d
1sreal <niibenedict123@gmail.com>
2023-01-18 23:54:17 +0000

    flag

---
+++ flag/gh_flag.py
@@ -0,0 +1,20 @@
+com_1 = input("Enter a number from one to two: \n")
+if com_1 == "1":
+    com_1a = int(input("Enter any number to know whether odd or even: \n"))
+    if com_1a % 2 == 1:
+        print("The number is odd")
+    else:
+         print("The number is even") 
+   
+if com_1 == "2":
+    com_2 = input("Enter any color from the Ghana flag: \n")
+    if com_2.lower() == "red":
+        print("It stand for the blood our forefathehrs. \n")
+    if com_2.lower() == "yellow":
+        print("It stands for our riches. \n")
+    if com_2.lower() == "green":
+        print("It stands for our agriculture. \n")
+    if com_2.lower() == "black":
+        print("It stands for our color and how special we are. \n")
+
+
