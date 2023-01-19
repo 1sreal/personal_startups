394888787316381fe39d84f34a3cd0791d7feffc
1sreal <niibenedict123@gmail.com>
2023-01-19 22:48:07 +0000

    simple_color_guess

---
+++ guess_color/guess_color.py
@@ -0,0 +1,13 @@
+riddles = {
+    "I am the color of the sky and the ocean. What am I?": "blue",
+    "I am the color of a stop sign. What am I?": "red",
+    "I am the color of grass. What am I?": "green",
+    "I am the color of a banana. What am I?": "yellow",
+    "I am the color of a flamingo. What am I?": "pink",
+    "I am the color of a cloud. What am I?": "white",
+    "I am the color of coal. What am I?": "black",
+    "I am the color of a pumpkin. What am I?": "orange",
+    "I am the color of a peacock. What am I?": "blue",
+    "I am the color of a cherry. What am I?": "red"
+}
+
