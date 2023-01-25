57e0f2e5cb6b33504e63209adb7e15b3e0e80c7a
1sreal <niibenedict123@gmail.com>
2023-01-25 23:54:26 +0000

    mcq_quiz

---
+++ coun_quiz.py
@@ -0,0 +1,23 @@
+questions = [
+    {
+        "question": "What is the capital of France?",
+        "options": ["Paris", "London", "New York", "Tokyo"],
+        "answer": "Paris"
+    },
+    {
+        "question": "Who is the inventor of the World Wide Web?",
+        "options": ["Bill Gates", "Mark Zuckerberg", "Steve Jobs", "Tim Berners-Lee"],
+        "answer": "Tim Berners-Lee"
+    },
+    {
+        "question": "Which planet is known as the Red Planet?",
+        "options": ["Saturn", "Jupiter", "Mars", "Venus"],
+        "answer": "Mars"
+    },
+    {
+        "question": "What is the largest ocean in the world?",
+        "options": ["Atlantic Ocean", "Pacific Ocean", "Indian Ocean", "Southern Ocean"],
+        "answer": "Pacific Ocean"
+    }
+]
+
