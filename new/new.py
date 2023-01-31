68edaf1d698bd37d4fc6d89823c331abbb9d44ad
1sreal <niibenedict123@gmail.com>
2023-01-31 23:38:07 +0000

    game

--- new/new.py
+++ new/new.py
@@ -8,6 +8,17 @@ def guess_the_number():
     number = random.randint(1, 100)
     attempts = 0
     
+    # Determine two clues for the number
+    if number % 2 == 0:
+        clue1 = "even"
+    else:
+        clue1 = "odd"
+        
+    if number >= 1 and number <= 50:
+        clue2 = "between 1 and 50"
+    else:
+        clue2 = "between 51 and 100"
+        
     while True:
         # Get player's guess
         guess = int(input("Enter your guess: "))
@@ -20,6 +31,7 @@ def guess_the_number():
             print("Too high, try again.")
         else:
             print("You got it in", attempts, "attempts!")
+            print("The number was", clue1, "and", clue2)
             play_again = input("Would you like to play again? (yes/no) ")
             
             # Use a switch case to handle user's answer
