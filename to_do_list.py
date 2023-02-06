0433d688222170bffbbdc7a1f5719bf8246ba43c
1sreal <niibenedict123@gmail.com>
2023-02-06 23:59:55 +0000

    to-do_list

--- to_do_list.py
+++ to_do_list.py
@@ -4,3 +4,18 @@
 # add ADT opperations such as editing, deleting, and updating
 # users should be able to mark lists as complete or incomplete
 
+# Create an empty dictionary to store the to-do list items
+todo_list = {}
+
+begin = input("WELCOME TO TO-DO LIST!!!\n Enter 1. To add to list.\n 2. To view list.\n q. To quit. ")
+while begin == "1":
+    input("Enter task (q to quit):")
+    if task == 'q':
+        break
+     
+    status = input("Enter task status (not started, in progress, done): ")
+    todo_list[task] = status
+while begin == "2":
+    print(todo_list)
+    
+
