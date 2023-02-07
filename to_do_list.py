83be8156ce6a798ffedd8741da93d850507fa962
1sreal <niibenedict123@gmail.com>
2023-02-07 23:47:51 +0000

    to-do list

--- to_do_list.py
+++ to_do_list.py
@@ -7,15 +7,23 @@
 # Create an empty dictionary to store the to-do list items
 todo_list = {}
 
-begin = input("WELCOME TO TO-DO LIST!!!\n Enter 1. To add to list.\n 2. To view list.\n q. To quit. ")
-while begin == "1":
-    input("Enter task (q to quit):")
-    if task == 'q':
+begin = input("WELCOME TO TO-DO LIST!!!\n Enter...\n 1. To add to list.\n")
+while True:
+    if begin == "1":
+        task = input("Enter new task ")
+        
+        
+        status = input("Enter task status (not started, in progress, done): ")
+        print( "\n " + task + ": " + status )
+        todo_list[task] = status
+        quit = input("input q to quit:")
+        if quit == 'q':
+            exit()
+    else:
+        print("Kindly read the instructions carefully and try again\n")
         break
-     
-    status = input("Enter task status (not started, in progress, done): ")
-    todo_list[task] = status
-while begin == "2":
-    print(todo_list)
+
+
     
+
 
